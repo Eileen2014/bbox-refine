f8297510eccbc46641b4ab8d2ce78c7fa70a1434
kvmanohar22 <kvmanohar22@gmail.com>
2019-01-10 00:13:02 +0530

    add options for detections from file

--- scripts/superpixels.sh
+++ scripts/superpixels.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-INPUT_DIR=/home/avisek/kv/datasets/VOC2012/JPEGImages
+INPUT_DIR=/home/avisek/kv/datasets/VOCdevkit/VOC2007/JPEGImages
 SUPERPIXELS=1000
-OUTPUT_DIR=/home/avisek/kv/datasets/VOC2012/superpixels/ers_400
+OUTPUT_DIR=/home/avisek/kv/datasets/VOCdevkit/VOC2007/superpixels/ers_400
 mkdir -p ${OUTPUT_DIR}
 
 cd ../../superpixels-revisited
